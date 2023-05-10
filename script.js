2ddad2b67f01ece05e94fec4a842590401315fc7
Brieditis <114050689+Brieditis@users.noreply.github.com>
2023-05-10 09:37:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const coolButton = document.querySelector('.js-cool');
+
+coolButton.addEventListener('click', () => {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley')
+})
+
+/* 'blank' ieliek jaunā cilnē (bet der arī bez :) ) */
